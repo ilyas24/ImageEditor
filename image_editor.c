8d53b274107f88f486b7f18634400c34d58fda26
Ilyas <ilyas@owners-MacBook-Pro.local>
2022-01-06 15:41:24 -0500

    Image Editor

--- image_editor.c
+++ image_editor.c
@@ -9,7 +9,6 @@
 void remove_red();
 void convert_to_black_and_white();
 void instagram_square();
-// void check_errors(int argc, char argv);
 
 int main(int argc, char *argv[]) {
         /* Task 1 needs to be completed here: Make sure that the command line arguments are correct. In case an error exists,
@@ -64,33 +63,3 @@ int main(int argc, char *argv[]) {
         return 0;
 }
 
-// void check_errors(int argc, char argv){
-//         // terminate program if there's more than one command-line argument
-//         if (argc > 2){
-//             printf("Error: Expecting one command-line argument, "
-//                 "which needs to be either 1, 2, or 3.");
-//             exit(1);
-//         }
-//         // users input is not a valid number
-//         if (atoi(argv[1]) == 0){
-//             printf("Error: Expecting one command-line argument, "
-//                 "which needs to be either 1, 2, or 3.");
-//             exit(1);
-//         }
-//         // check for decimals or combinations of words and numbers
-//         char *extra;
-//         strtol(argv[1], &extra, 10);
-//         // if there are extra characters, terminate program
-//         if (*extra != '\0'){
-//             printf("Error: Expecting one command-line argument, "
-//                 "which needs to be either 1, 2, or 3.");
-//             exit(1);
-//         }
-//         // input numbers aren't 1, 2, or 3
-//         if (atoi(argv[1]) != 1 && atoi(argv[1]) != 2 && 
-//             atoi(argv[1]) != 3 ){
-//             printf("Error: Expecting one command-line argument, "
-//                 "which needs to be either 1, 2, or 3.");
-//             exit(1);
-//         }
-//    }
